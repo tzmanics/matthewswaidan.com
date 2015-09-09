8b28474cc42a7691c3f8b5c0b1c02a426ff95927
Tara Z. Manicsic <tara@modulus.io>
2015-09-09 10:41:43 -0400

    Fixes spacing for css & js files.

--- js/posts_add.js
+++ js/posts_add.js
@@ -1,13 +1,14 @@
 // setup options for ajax
 var options = {
-	type: 'POST',
-	url: '/posts/p_add/',
-	beforeSubmit: function(){
-		$('#results').html("Adding...");
-	},
-	success: function(reponse){
-		$('#results').html("Your post was added.");
-	}
+ type: 'POST',
+ url: '/posts/p_add/',
+ beforeSubmit: function () {
+   $('#results').html("Adding...");
+ },
+
+ success: function (reponse) {
+   $('#results').html("Your post was added.");
+ }
 };
 
 // using the above ajax the form
